e68d1e58e7c3bdd67515a72f7a63f55f427c4c33
KRichard-progm <76790600+KRichard-progm@users.noreply.github.com>
2021-01-09 15:16:35 +0100

    Add files via upload

---
+++ f3.js
@@ -0,0 +1,6 @@
+let Celsius = window.prompt("Celsius?");
+let f = Number.parseInt(Celsius)*1.8 + 32;
+alert(f);
+let Fahrenheit = window.prompt("Fahrenheit?");
+let c = (Number.parseInt(Fahrenheit)-32) / 1.8;
+alert(c)
